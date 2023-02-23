51f0482977044362b7cd7d043d8f692d7745075e
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2023-02-23 21:56:49 +0400

    Formatting

--- cli.pas
+++ cli.pas
@@ -4,91 +4,89 @@ program RoadNetworkEfficiency;
 uses SysUtils, LatLng;
 
 type 
-	Arguments = record
-		FromLatLng: string;
-		ToLatLng: string;
-	end;  
-	PArguments = ^Arguments;
-	EInvalidArguments = Class(Exception);
+  Arguments = record
+    ShowHelp: Boolean;
+    FromLatLng: string;
+    ToLatLng: string;
+  end;  
+  PArguments = ^Arguments;
+  EInvalidArguments = Class(Exception);
 
 procedure WriteHelp;
 begin
-	WriteLn('Road Network Efficiency');
-	WriteLn('-----------------------');
-	WriteLn('Calculates how tortuous the road from one latlng is to another');
-	WriteLn('');
-	WriteLn('USAGE:');
-	WriteLn('./RoadNetworkEfficiency <options>');
-	WriteLn('');
-	WriteLn('OPTIONS');
-	WriteLn('-h				: Prints help');
-	WriteLn('-from [FromLatLng]		: Starting Latitude and Longitude');
-	WriteLn('-to [ToLatLng]			: Destination Latitutde and Longitude');
-end;
-
-procedure ValidateArguments(args: PArguments);
-begin
-	if args = nil then begin
-		raise EInvalidArguments.Create('No arguments supplied');
-	end;
-	if Length(args^.FromLatLng) = 0 then begin
-		raise EInvalidArguments.Create('-from LatLng not set');
-	end;
-	if Length(args^.ToLatLng) = 0 then begin
-		raise EInvalidArguments.Create('-to LatLng not set');
-	end;
+  WriteLn('Road Network Efficiency');
+  WriteLn('-----------------------');
+  WriteLn('Calculates how tortuous the road from one latlng is to another');
+  WriteLn('');
+  WriteLn('USAGE:');
+  WriteLn('./RoadNetworkEfficiency <options>');
+  WriteLn('');
+  WriteLn('OPTIONS');
+  WriteLn('-h       : Prints help');
+  WriteLn('-from [FromLatLng]   : Starting Latitude and Longitude');
+  WriteLn('-to [ToLatLng]     : Destination Latitutde and Longitude');
 end;
 
 procedure ParseArguments(args: PArguments);
 var
-	ShowHelp: Boolean;
-	ArgumentName: String;
-	ArgumentValue: String;
-	Count: Integer;
+  ArgumentName: String;
+  ArgumentValue: String;
+  Count: Integer;
 begin
-	ShowHelp := (ParamCount = 0) or ((ParamCount = 1) and (ParamStr(1) = '-h'));
-	if ShowHelp then begin
-		WriteHelp;
-		halt;
-	end;
-	for Count := 1 to ParamCount do begin
-		if pos('-', ParamStr(Count)) = 1 then ArgumentName := ParamStr(Count)
-		else begin
-			ArgumentValue := ParamStr(Count);
-			with args^ do begin
-				case (ArgumentName) of
-					'-from': FromLatLng := ArgumentValue;
-					'-to': ToLatLng := ArgumentValue;
-				end;
-			end;
-		end;
-	end;
-	ValidateArguments(args);
+  for Count := 1 to ParamCount do begin
+    if pos('-', ParamStr(Count)) = 1 then ArgumentName := ParamStr(Count)
+    else begin
+      ArgumentValue := ParamStr(Count);
+      with args^ do begin
+        case (ArgumentName) of
+          '-h': ShowHelp := true;
+          '-from': FromLatLng := ArgumentValue;
+          '-to': ToLatLng := ArgumentValue;
+        end;
+      end;
+    end;
+  end;
+end;
+
+procedure ValidateArguments(args: PArguments);
+begin
+  if args = nil then begin
+    raise EInvalidArguments.Create('No arguments supplied');
+  end;
+  if Length(args^.FromLatLng) = 0 then begin
+    raise EInvalidArguments.Create('-from LatLng not set');
+  end;
+  if Length(args^.ToLatLng) = 0 then begin
+    raise EInvalidArguments.Create('-to LatLng not set');
+  end;
 end;
 
 var 
-	args: PArguments;
-	
-	fromLatLng: TLatLng;
-	toLatLng: TLatLng;
+  args: PArguments;
+  
+  fromLatLng: TLatLng;
+  toLatLng: TLatLng;
 begin
-	New(args);
-	try
-		try
-			ParseArguments(args);
-			fromLatLng := LatLngFromStr(args^.FromLatLng);
-			toLatLng := LatLngFromStr(args^.ToLatLng);
+  New(args);
+  try
+    try
+      
+      ParseArguments(args);
+    
+      if args^.ShowHelp then WriteHelp
+      else ValidateArguments(args);
 
-			
+      fromLatLng := LatLngFromStr(args^.FromLatLng);
+      toLatLng := LatLngFromStr(args^.ToLatLng);
 
-		except
-	    	on E : EInvalidArguments do begin
-	    		WriteLn(e.Message);
-	    		WriteHelp;
-	    	end;
-	    	on E : Exception do WriteLn(e.Message);
-		end;
-	finally
-		Dispose(args);
-	end;
+    except
+        on E : EInvalidArguments do begin
+          WriteLn(e.Message);
+          WriteHelp;
+        end;
+        on E : Exception do WriteLn(e.Message);
+    end;
+  finally
+    Dispose(args);
+  end;
 end.
--- src/latlng.pas
+++ src/latlng.pas
@@ -24,12 +24,12 @@ var
 begin
   Tokens := s.Split(',');
   with Result do begin
-  	try
-	    Latitude := StrToFloat(Tokens[0]);
-	    Longitude := StrToFloat(Tokens[1]);
-	except
-		on E: Exception do raise EInvalidLatLng.Create('"'+s+'" is not a valid LatLng. A LatLng should be two decimals separated by a comma e.g. "12.32,34.56"');
-	end;
+    try
+      Latitude := StrToFloat(Tokens[0]);
+      Longitude := StrToFloat(Tokens[1]);
+  except
+    on E: Exception do raise EInvalidLatLng.Create('"'+s+'" is not a valid LatLng. A LatLng should be two decimals separated by a comma e.g. "12.32,34.56"');
+  end;
   end;
 end;
 
